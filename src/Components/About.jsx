6ab2be1cfd4f29de42e5b1b5ce6e32d48601728c
mohnish03 <agrawalmohnish0003@gmail.com>
2025-02-19 11:09:18 +0000

    c2

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,7 +16,7 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/DSCN0665.JPG";
+import image from "../images/mp.jpeg";
 
 const imageAltText = "purple and blue abstract background";
 
